4b8c5bf3a4ac79e074d512c0b1553f1d88c0d4fa
sjtype <31806705+sjtype@users.noreply.github.com>
2019-01-21 21:22:24 -0500

    added typescript support

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,11 +1,12 @@
 // IIFE -- Immediately Invoked Function Expression
 (function () {
     function Start() {
-        var student;
+        let student: objects.Student;
+        
         student = new objects.Student(20, "David", "D123456789");
         student.saysHello();
         student.studies();
     }
+    
     window.addEventListener("load", Start);
 })();
-//# sourceMappingURL=app.js.map
